1e2462712c18d979af6f92be841737e3fc63f07a
Richard Velásquez <rjvelasq@gmail.com>
2022-02-28 16:59:22 -0400

    first commit

---
+++ start.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+
+
+const PORT = process.env.PORT || 3000;
+const config = require('./config');
+if (config.credentials.client_id == null || config.credentials.client_secret == null) {
+    console.error('Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables.');
+    return;
+}
+
+let app = express();
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: '50mb' }));
+app.use('/api/forge/oauth', require('./routes/oauth'));
+app.use('/api/forge/oss', require('./routes/oss'));
+app.use('/api/forge/modelderivative', require('./routes/modelderivative'));
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.statusCode).json(err);
+});
+app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
+
