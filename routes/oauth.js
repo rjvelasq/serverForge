ec17e82d5175fb25d11c841c0e1e5788805a30f2
JoaoMartins-Forge <joao.martins@autodesk.com>
2022-02-28 18:56:17 -0300

    missing public token

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,6 +7,7 @@ let router = express.Router();
 // GET /api/forge/oauth/token - generates a public access token (required by the Forge viewer).
 router.get('/token', async (req, res, next) => {
     try {
+        const token = await getPublicToken();
          res.json({
             access_token: token.access_token,
             expires_in: token.expires_in
